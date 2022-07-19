6e8e11c691572c22444cc6f09349eae052c8895c
RichardRozin <richardwkrozin@gmail.com>
2022-07-18 21:33:06 -0400

    Update ex031.py

--- Exercicios Python Curso em Video/Mundo 1/ex031.py
+++ Exercicios Python Curso em Video/Mundo 1/ex031.py
@@ -1,6 +1,6 @@
 '''Exercício Python 031: Desenvolva um programa que pergunte a distância de uma viagem em Km.
 Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até
-200Km e R$0,45 parta viagens mais longas.'''
+200Km e R$0,45 para viagens mais longas.'''
 d = int(input('Qual é a distancia da viagem? '))
 print(f'Voce esta prestes a comecar uma viagem de {d:.1f}Km.')
 if d <= 200:
